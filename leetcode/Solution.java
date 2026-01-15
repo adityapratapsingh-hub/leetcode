f3ddbcc558145e186e883bc9e395d246a9a5bf85
Aditya Pratap Singh <kumar183408@gmail.com>
2026-01-16 01:00:29 +0530

    Add files via upload

---
+++ leetcode/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num=x;
+        int rev=0;
+        if(x<0){
+        return false;
+        }
+        while(x>0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+
+        }
+        return rev==num;
+        
+    }
+}
